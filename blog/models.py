78b93401f3667a1f1b2bf74373b9c1ac01665a6d
Dawit-y <dawityimer52@gmail.com>
2024-02-22 21:11:34 +0300

    organize models

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,13 @@ class BlogIndexPage(Page):
         FieldPanel('intro'),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context["blogpages"] = blogpages
+        return context
+        
+
 
 class BlogPage(Page):
     date = models.DateField("Post date")
